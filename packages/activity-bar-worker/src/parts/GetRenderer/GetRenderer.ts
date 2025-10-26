864964f4c06566f53b5bb2e86eed1295358b1396
Le Vivilet <levivilet@pm.me>
2025-10-26 18:17:50 +0100

    feature: add css renderer

--- packages/activity-bar-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/activity-bar-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -1,5 +1,6 @@
 import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
+import { renderCss } from '../RenderCss/RenderCss.ts'
 import * as RenderFocusContext from '../RenderFocusContext/RenderFocusContext.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
 
@@ -9,6 +10,10 @@ export const getRenderer = (diffType: number): Renderer => {
       return RenderItems.renderItems
     case DiffType.RenderFocusContext:
       return RenderFocusContext.renderFocusContext
+    case DiffType.RenderFocus:
+      return RenderFocusContext.renderFocusContext
+    case DiffType.RenderCss:
+      return renderCss
     default:
       throw new Error('unknown renderer')
   }
---
+++ packages/activity-bar-worker/src/parts/RenderCss/RenderCss.ts
@@ -0,0 +1,5 @@
+import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+
+export const renderCss = (oldState: ActivityBarState, newState: ActivityBarState): readonly any[] => {
+  return []
+}
