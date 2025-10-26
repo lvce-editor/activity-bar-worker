571addfd2a096f14bad5573b8a1542439bc3cef9
Le Vivilet <levivilet@pm.me>
2025-10-26 18:04:14 +0100

    feature: update applyRender

--- packages/activity-bar-worker/src/parts/ApplyRender/ApplyRender.ts
+++ packages/activity-bar-worker/src/parts/ApplyRender/ApplyRender.ts
@@ -1,6 +1,14 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+import * as GetRenderer from '../GetRenderer/GetRenderer.ts'
 
 export const applyRender = (oldState: ActivityBarState, newState: ActivityBarState, diffResult: readonly number[]): readonly any[] => {
-  const commands: any[] = []
+  const commands = []
+  for (const item of diffResult) {
+    const fn = GetRenderer.getRenderer(item)
+    const result = fn(oldState, newState)
+    if (result.length > 0) {
+      commands.push(result)
+    }
+  }
   return commands
 }
