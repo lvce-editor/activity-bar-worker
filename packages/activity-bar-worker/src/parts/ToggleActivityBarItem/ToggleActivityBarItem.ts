58f951daa656f2d81472ce8125ac6e92367e2620
Le Vivilet <levivilet@pm.me>
2025-10-26 13:43:33 +0100

    feature: add toggle function (#17)

---
+++ packages/activity-bar-worker/src/parts/ToggleActivityBarItem/ToggleActivityBarItem.ts
@@ -0,0 +1,14 @@
+import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+
+const isEnabled = (activityBarItem: any): boolean => {
+  return activityBarItem.enabled
+}
+
+export const toggleActivityBarItem = async (state: ActivityBarState, item: any): Promise<ActivityBarState> => {
+  const activityBarItem = state.activityBarItems.find((activityBarItem) => activityBarItem.id === item.label)
+  activityBarItem.enabled = !activityBarItem.enabled
+  return {
+    ...state,
+    activityBarItems: state.activityBarItems.filter(isEnabled),
+  }
+}
