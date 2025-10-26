f35fff3adf28f4283733c98a5a965d52c21c8061
Le Vivilet <levivilet@pm.me>
2025-10-26 18:37:07 +0100

    feature: add string test (#69)

---
+++ packages/activity-bar-worker/test/ActivityBarStrings.test.ts
@@ -0,0 +1,46 @@
+import { test, expect } from '@jest/globals'
+import * as ActivityBarStrings from '../src/parts/ActivityBarStrings/ActivityBarStrings.ts'
+
+test('ActivityBarStrings.explorer should return correct string', () => {
+  expect(ActivityBarStrings.explorer()).toBe('Explorer')
+})
+
+test('ActivityBarStrings.search should return correct string', () => {
+  expect(ActivityBarStrings.search()).toBe('Search')
+})
+
+test('ActivityBarStrings.sourceControl should return correct string', () => {
+  expect(ActivityBarStrings.sourceControl()).toBe('Source Control')
+})
+
+test('ActivityBarStrings.runAndDebug should return correct string', () => {
+  expect(ActivityBarStrings.runAndDebug()).toBe('Run and Debug')
+})
+
+test('ActivityBarStrings.extensions should return correct string', () => {
+  expect(ActivityBarStrings.extensions()).toBe('Extensions')
+})
+
+test('ActivityBarStrings.settings should return correct string', () => {
+  expect(ActivityBarStrings.settings()).toBe('Settings')
+})
+
+test('ActivityBarStrings.additionalViews should return correct string', () => {
+  expect(ActivityBarStrings.additionalViews()).toBe('Additional Views')
+})
+
+test('ActivityBarStrings.activityBar should return correct string', () => {
+  expect(ActivityBarStrings.activityBar()).toBe('Activity Bar')
+})
+
+test('ActivityBarStrings.moveSideBarRight should return correct string', () => {
+  expect(ActivityBarStrings.moveSideBarRight()).toBe('Move Side Bar Right')
+})
+
+test('ActivityBarStrings.moveSideBarLeft should return correct string', () => {
+  expect(ActivityBarStrings.moveSideBarLeft()).toBe('Move Side Bar Left')
+})
+
+test('ActivityBarStrings.hideActivityBar should return correct string', () => {
+  expect(ActivityBarStrings.hideActivityBar()).toBe('Hide Activity Bar')
+})
