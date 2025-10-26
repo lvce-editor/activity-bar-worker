7495fd188d1b08cc0aa9256a41432e875189a541
Le Vivilet <levivilet@pm.me>
2025-10-26 12:55:57 +0100

    feature: add handleClick

--- packages/activity-bar-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/activity-bar-worker/src/parts/CommandMap/CommandMap.ts
@@ -7,6 +7,7 @@ import * as FocusLast from '../FocusLast/FocusLast.ts'
 import * as FocusNext from '../FocusNext/FocusNext.ts'
 import * as FocusNone from '../FocusNone/FocusNone.ts'
 import { getKeyBindings } from '../GetKeyBindings/GetKeyBindings.ts'
+import * as HandleClick from '../HandleClick/HandleClick.ts'
 import * as Render2 from '../Render2/Render2.ts'
 import * as RenderEventListeners from '../RenderEventListeners/RenderEventListeners.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
@@ -19,6 +20,7 @@ export const commandMap = {
   'ActivityBar.focusNext': WrapCommand.wrapCommand(FocusNext.focusNext),
   'ActivityBar.focusNone': WrapCommand.wrapCommand(FocusNone.focusNone),
   'ActivityBar.getKeyBindings': getKeyBindings,
+  'ActivityBar.handleClick': WrapCommand.wrapCommand(HandleClick.handleClick),
   'ActivityBar.getCommandIds': WrapCommand.getCommandIds,
   'ActivityBar.render2': Render2.render2,
   'ActivityBar.renderEventListeners': RenderEventListeners.renderEventListeners,
---
+++ packages/activity-bar-worker/src/parts/HandleClick/HandleClick.ts
@@ -0,0 +1,49 @@
+import { MenuEntryId, MouseEventType } from '@lvce-editor/constants'
+import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+
+const handleClickSettings = async (state: ActivityBarState, x: number, y: number, viewletId: string) => {
+  await ContextMenu.show(x, y, MenuEntryId.Settings)
+  return state
+}
+
+const handleClickAdditionalViews = async (state: ActivityBarState, x: number, y: number, viewletId: string) => {
+  await ContextMenu.show(x, y, MenuEntryId.ActivityBarAdditionalViews)
+  return state
+}
+
+const handleClickOther = async (state: ActivityBarState, x: number, y: number, viewletId: string) => {
+  // TODO ask renderer worker asynchronously if sidebar is visible
+
+  const { sideBarVisible, currentViewletId } = state
+  if (sideBarVisible) {
+    if (currentViewletId === viewletId) {
+      await RendererWorker.invoke('Layout.hideSideBar')
+    } else {
+      await RendererWorker.invoke(/* SideBar.show */ 'SideBar.show', /* id */ viewletId)
+    }
+  } else {
+    // TODO should show side bar with viewletId
+    // @ts-ignore
+    await RendererWorker.invoke('Layout.showSideBar')
+  }
+  return state
+}
+
+export const handleClick = async (state: ActivityBarState, button: number, index: number, x: number, y: number): Promise<any> => {
+  if (button !== MouseEventType.LeftClick) {
+    return state
+  }
+  const { activityBarItems } = state
+  const item = activityBarItems[index]
+  const viewletId = item.id
+  switch (viewletId) {
+    case 'Settings':
+      return handleClickSettings(state, x, y, viewletId)
+    case 'Additional Views':
+      return handleClickAdditionalViews(state, x, y, viewletId)
+    default:
+      return handleClickOther(state, x, y, viewletId)
+  }
+}
