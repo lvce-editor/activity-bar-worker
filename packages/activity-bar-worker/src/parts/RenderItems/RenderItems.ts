eef0f09be7d3d038133ea43cd792c61ef95b447e
Le Vivilet <levivilet@pm.me>
2025-10-26 18:24:34 +0100

    feature: render dom (#57)

--- packages/activity-bar-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/activity-bar-worker/src/parts/RenderItems/RenderItems.ts
@@ -1,6 +1,9 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+import { getActivityBarVirtualDom } from '../GetActivityBarVirtualDom/GetActivityBarVirtualDom.ts'
 
 export const renderItems = (oldState: ActivityBarState, newState: ActivityBarState): any => {
-  return [ViewletCommand.SetDom2, []]
+  const { uid, activityBarItems } = newState
+  const dom = getActivityBarVirtualDom(activityBarItems)
+  return [ViewletCommand.SetDom2, uid, dom]
 }
