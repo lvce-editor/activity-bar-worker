bcbcc39d017995d6e07e801ee1ccdd2119fa78d9
Le Vivilet <levivilet@pm.me>
2025-10-26 18:04:34 +0100

    feature: add activity bar dom (#52)

---
+++ packages/activity-bar-worker/src/parts/GetActivityBarVirtualDom/GetActivityBarVirtualDom.ts
@@ -0,0 +1,30 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles } from '@lvce-editor/constants'
+import { mergeClassNames } from '@lvce-editor/virtual-dom-worker'
+import * as ActivityBarStrings from '../ActivityBarStrings/ActivityBarStrings.ts'
+import * as AriaOrientationType from '../AriaOrientationType/AriaOrientationType.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as DomId from '../DomId/DomId.ts'
+import * as GetActivityBarItemsVirtualDom from '../GetActivityBarItemsVirtualDom/GetActivityBarItemsVirtualDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getActivityBarVirtualDom = (visibleItems: readonly any[]): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      id: DomId.ActivityBar,
+      className: mergeClassNames(ClassNames.Viewlet, ClassNames.ActivityBar),
+      role: AriaRoles.ToolBar,
+      ariaRoleDescription: ActivityBarStrings.activityBar(),
+      ariaOrientation: AriaOrientationType.Vertical,
+      tabIndex: 0,
+      onMouseDown: DomEventListenerFunctions.HandleMouseDown,
+      onContextMenu: DomEventListenerFunctions.HandleContextMenu,
+      onFocus: DomEventListenerFunctions.HandleFocus,
+      onBlur: DomEventListenerFunctions.HandleBlur,
+      childCount: visibleItems.length,
+    },
+    ...GetActivityBarItemsVirtualDom.getVirtualDom(visibleItems),
+  ]
+}
