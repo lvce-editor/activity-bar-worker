b5a22e02d9b888c637b25add1b452867662d6442
Le Vivilet <levivilet@pm.me>
2025-10-26 12:59:36 +0100

    lint

--- packages/activity-bar-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/activity-bar-worker/src/parts/HandleClick/HandleClick.ts
@@ -3,17 +3,17 @@ import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
-const handleClickSettings = async (state: ActivityBarState, x: number, y: number, viewletId: string) => {
+const handleClickSettings = async (state: ActivityBarState, x: number, y: number, viewletId: string): Promise<ActivityBarState> => {
   await ContextMenu.show(x, y, MenuEntryId.Settings)
   return state
 }
 
-const handleClickAdditionalViews = async (state: ActivityBarState, x: number, y: number, viewletId: string) => {
+const handleClickAdditionalViews = async (state: ActivityBarState, x: number, y: number, viewletId: string): Promise<ActivityBarState> => {
   await ContextMenu.show(x, y, MenuEntryId.ActivityBarAdditionalViews)
   return state
 }
 
-const handleClickOther = async (state: ActivityBarState, x: number, y: number, viewletId: string) => {
+const handleClickOther = async (state: ActivityBarState, x: number, y: number, viewletId: string): Promise<ActivityBarState> => {
   // TODO ask renderer worker asynchronously if sidebar is visible
 
   const { sideBarVisible, currentViewletId } = state
@@ -31,7 +31,7 @@ const handleClickOther = async (state: ActivityBarState, x: number, y: number, v
   return state
 }
 
-export const handleClick = async (state: ActivityBarState, button: number, index: number, x: number, y: number): Promise<any> => {
+export const handleClick = async (state: ActivityBarState, button: number, index: number, x: number, y: number): Promise<ActivityBarState> => {
   if (button !== MouseEventType.LeftClick) {
     return state
   }
