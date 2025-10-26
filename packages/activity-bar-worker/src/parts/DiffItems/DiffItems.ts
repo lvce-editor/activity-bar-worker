3183b87b11ad16dfe0a0287594b3068d91e45b73
Le Vivilet <levivilet@pm.me>
2025-10-26 18:11:09 +0100

    feature: improve items diff

--- packages/activity-bar-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/activity-bar-worker/src/parts/DiffItems/DiffItems.ts
@@ -1,5 +1,5 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 
 export const isEqual = (oldState: ActivityBarState, newState: ActivityBarState): boolean => {
-  return oldState.focusedIndex === newState.focusedIndex
+  return oldState.focusedIndex === newState.focusedIndex && oldState.activityBarItems === newState.activityBarItems
 }
