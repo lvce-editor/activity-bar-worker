43f85b393d5cde324a8d46dece1a4dd93efe7626
Le Vivilet <levivilet@pm.me>
2025-10-26 13:45:29 +0100

    feature: add findIndex (#20)

---
+++ packages/activity-bar-worker/src/parts/FindIndex/FindIndex.ts
@@ -0,0 +1,8 @@
+export const findIndex = (activityBarItems: readonly any[], id: string): number => {
+  for (let i = 0; i < activityBarItems.length; i++) {
+    if (activityBarItems[i].id === id) {
+      return i
+    }
+  }
+  return -1
+}
