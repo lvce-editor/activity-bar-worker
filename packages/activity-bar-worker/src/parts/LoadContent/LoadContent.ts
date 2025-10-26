5453577d861e21c099f05ff3defc3f0b5fb69d9d
Le Vivilet <levivilet@pm.me>
2025-10-26 18:20:17 +0100

    feature: load items

--- packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,7 +1,10 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+import { getActivityBarItems } from '../GetActivityBarItems/GetActivityBarItems.ts'
 
 export const loadContent = async (state: ActivityBarState, savedState: any): Promise<ActivityBarState> => {
+  const items = getActivityBarItems()
   return {
     ...state,
+    activityBarItems: items,
   }
 }
