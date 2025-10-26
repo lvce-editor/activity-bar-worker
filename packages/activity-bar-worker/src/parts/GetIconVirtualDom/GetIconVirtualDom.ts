1315e324fdd84eb9ff2480a808e5c91091b90d04
Le Vivilet <levivilet@pm.me>
2025-10-26 17:57:37 +0100

    feature: add icon dom

---
+++ packages/activity-bar-worker/src/parts/GetIconVirtualDom/GetIconVirtualDom.ts
@@ -0,0 +1,12 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { AriaRoles } from '@lvce-editor/constants'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getIconVirtualDom = (icon: string, type = VirtualDomElements.Div): VirtualDomNode => {
+  return {
+    type,
+    className: `MaskIcon MaskIcon${icon}`,
+    role: AriaRoles.None,
+    childCount: 0,
+  }
+}
