932a80eba5c6412ed46b0eb65fd509d859882458
Le Vivilet <levivilet@pm.me>
2025-10-26 12:40:59 +0100

    feature: add strings

---
+++ packages/activity-bar-worker/src/parts/ActivityBarStrings/ActivityBarStrings.ts
@@ -0,0 +1,46 @@
+import * as I18nString from '../I18NString/I18NString.ts'
+import * as UiStrings from '../UiStrings/UiStrings.ts'
+
+export const explorer = () => {
+  return I18nString.i18nString(UiStrings.Explorer)
+}
+
+export const search = () => {
+  return I18nString.i18nString(UiStrings.Search)
+}
+
+export const sourceControl = () => {
+  return I18nString.i18nString(UiStrings.SourceControl)
+}
+
+export const runAndDebug = () => {
+  return I18nString.i18nString(UiStrings.RunAndDebug)
+}
+
+export const extensions = () => {
+  return I18nString.i18nString(UiStrings.Extensions)
+}
+
+export const settings = () => {
+  return I18nString.i18nString(UiStrings.Settings)
+}
+
+export const additionalViews = () => {
+  return I18nString.i18nString(UiStrings.AdditionalViews)
+}
+
+export const activityBar = () => {
+  return I18nString.i18nString(UiStrings.ActivityBar)
+}
+
+export const moveSideBarRight = () => {
+  return I18nString.i18nString(UiStrings.MoveSideBarRight)
+}
+
+export const moveSideBarLeft = () => {
+  return I18nString.i18nString(UiStrings.MoveSideBarLeft)
+}
+
+export const hideActivityBar = () => {
+  return I18nString.i18nString(UiStrings.HideActivityBar)
+}
