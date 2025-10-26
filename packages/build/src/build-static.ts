351f731098b490ea852e685db0c3d69f5953e99b
Le Vivilet <levivilet@pm.me>
2025-10-26 11:45:26 +0100

    build

--- packages/build/src/build-static.ts
+++ packages/build/src/build-static.ts
@@ -1,4 +1,4 @@
-import { cp, readFile, writeFile } from 'node:fs/promises'
+import { cp, readFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.ts'
@@ -23,17 +23,19 @@ export const getRemoteUrl = (path: string): string => {
   return `/remote/${url}`
 }
 
+// @ts-ignore
 const content = await readFile(rendererWorkerPath, 'utf8')
 const workerPath = join(root, '.tmp/dist/dist/activityBarWorkerMain.js')
+// @ts-ignore
 const remoteUrl = getRemoteUrl(workerPath)
 
-const occurrence = `// const explorerWorkerUrl = \`\${assetDir}/packages/explorer-worker/dist/activityBarWorkerMain.js\`
-const explorerWorkerUrl = \`${remoteUrl}\``
-const replacement = `const explorerWorkerUrl = \`\${assetDir}/packages/explorer-worker/dist/activityBarWorkerMain.js\``
-if (!content.includes(occurrence)) {
-  throw new Error('occurrence not found')
-}
-const newContent = content.replace(occurrence, replacement)
-await writeFile(rendererWorkerPath, newContent)
+// const occurrence = `// const explorerWorkerUrl = \`\${assetDir}/packages/explorer-worker/dist/activityBarWorkerMain.js\`
+// const explorerWorkerUrl = \`${remoteUrl}\``
+// const replacement = `const explorerWorkerUrl = \`\${assetDir}/packages/explorer-worker/dist/activityBarWorkerMain.js\``
+// if (!content.includes(occurrence)) {
+//   throw new Error('occurrence not found')
+// }
+// const newContent = content.replace(occurrence, replacement)
+// await writeFile(rendererWorkerPath, newContent)
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
