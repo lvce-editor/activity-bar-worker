1dc9edb82f929ee0ec060527f8512ac216d2824a
Le Vivilet <levivilet@pm.me>
2025-10-27 10:48:49 +0100

    feature: add context menu e2e test (#97)

---
+++ packages/e2e/src/activity-bar.context-menu.ts
@@ -0,0 +1,15 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'activity-bar.context-menu'
+
+export const skip = 1
+
+export const test: Test = async ({ ActivityBar, Locator, expect }) => {
+  // act
+  await ActivityBar.handleContextMenu()
+
+  // assert
+  const explorer = Locator('.ActivityBarItem[title="Search"]')
+  await expect(explorer).toBeVisible()
+  await expect(explorer).toHaveClass('IconSearch')
+}
