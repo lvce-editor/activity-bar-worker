1bab196ea07213046008e3141c0d9113bcaa2e4b
Le Vivilet <levivilet@pm.me>
2025-10-27 14:44:21 +0100

    rename

--- packages/activity-bar-worker/src/parts/Diff2/Diff2.ts
+++ packages/activity-bar-worker/src/parts/Diff2/Diff2.ts
@@ -1,6 +1,6 @@
-import * as ExplorerStates from '../ActivityBarStates/ActivityBarStates.ts'
+import * as ActivityBarStates from '../ActivityBarStates/ActivityBarStates.ts'
 import * as DiffModules from '../DiffModules/DiffModules.ts'
 
 export const diff2 = (uid: number): readonly number[] => {
-  return ExplorerStates.diff(uid, DiffModules.modules, DiffModules.numbers)
+  return ActivityBarStates.diff(uid, DiffModules.modules, DiffModules.numbers)
 }
