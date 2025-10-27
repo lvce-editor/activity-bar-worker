37f49894bb3f4286831da65878d9d349eb24f7b9
Le Vivilet <levivilet@pm.me>
2025-10-27 10:36:56 +0100

    index

--- packages/activity-bar-worker/src/parts/GetIndexFromPosition/GetIndexFromPosition.ts
+++ packages/activity-bar-worker/src/parts/GetIndexFromPosition/GetIndexFromPosition.ts
@@ -1,3 +1,7 @@
 export const getIndexFromPosition = (eventX: number, eventY: number, itemHeight: number, itemCount: number): number => {
-  return -1
+  const index = Math.floor(eventY / itemHeight)
+  if (index < 0 || index >= itemCount) {
+    return -1
+  }
+  return index
 }
