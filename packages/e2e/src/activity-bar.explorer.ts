a169b4580f7b580ed31a1f9654e9eecd0f8373c5
Le Vivilet <levivilet@pm.me>
2025-10-27 10:41:04 +0100

    feature: add activity bar explorer e2e test (#90)

---
+++ packages/e2e/src/activity-bar.explorer.ts
@@ -0,0 +1,10 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'activity-bar.explorer'
+
+export const test: Test = async ({ Locator, expect }) => {
+  // assert
+  const explorer = Locator('.ActivityBarItem[title="Explorer"]')
+  await expect(explorer).toBeVisible()
+  await expect(explorer).toHaveClass('IconFiles')
+}
