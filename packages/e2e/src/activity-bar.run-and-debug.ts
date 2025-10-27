760b8ce508df9e0f2439e9e16456e520e0731523
Le Vivilet <levivilet@pm.me>
2025-10-27 10:44:47 +0100

    feature: add debug e2e test

---
+++ packages/e2e/src/activity-bar.run-and-debug.ts
@@ -0,0 +1,10 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'activity-bar.run-and-debug'
+
+export const test: Test = async ({ Locator, expect }) => {
+  // assert
+  const explorer = Locator('.ActivityBarItem[title="Run and Debug"]')
+  await expect(explorer).toBeVisible()
+  await expect(explorer).toHaveClass('IconDebugAlt2')
+}
