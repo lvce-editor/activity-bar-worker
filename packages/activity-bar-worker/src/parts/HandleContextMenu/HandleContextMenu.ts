26ac8c26c727039fe70172f36d71a4c2015ea6cd
Le Vivilet <levivilet@pm.me>
2025-10-27 10:49:48 +0100

    feature: add handleContextMenu

---
+++ packages/activity-bar-worker/src/parts/HandleContextMenu/HandleContextMenu.ts
@@ -0,0 +1,8 @@
+import { MenuEntryId } from '@lvce-editor/constants'
+import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+import * as ContextMenu from '../ContextMenu/ContextMenu.js'
+
+export const handleContextMenu = async (state: ActivityBarState, button: number, x: number, y: number): Promise<ActivityBarState> => {
+  await ContextMenu.show(x, y, MenuEntryId.ActivityBar)
+  return state
+}
