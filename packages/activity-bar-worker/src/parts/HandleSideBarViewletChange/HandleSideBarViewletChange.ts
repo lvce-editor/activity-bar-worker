c3eb3533e89ee4dd90134fcdc099e254593b9bd1
Le Vivilet <levivilet@pm.me>
2025-10-27 11:40:13 +0100

    feature: use destructuring (#105)

--- packages/activity-bar-worker/src/parts/HandleSideBarViewletChange/HandleSideBarViewletChange.ts
+++ packages/activity-bar-worker/src/parts/HandleSideBarViewletChange/HandleSideBarViewletChange.ts
@@ -2,7 +2,8 @@ import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import { findIndex } from '../FindIndex/FindIndex.ts'
 
 export const handleSideBarViewletChange = (state: ActivityBarState, id: string, ...args: readonly any[]): ActivityBarState => {
-  const index = findIndex(state.activityBarItems, id)
+  const { activityBarItems } = state
+  const index = findIndex(activityBarItems, id)
   return {
     ...state,
     selectedIndex: index,
