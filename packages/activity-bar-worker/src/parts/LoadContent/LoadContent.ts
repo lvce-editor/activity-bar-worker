8e4b531c6a03124a0aef75087dc27373241e40ba
Le Vivilet <levivilet@pm.me>
2025-10-27 18:21:14 +0100

    feature: mark item in loadContent active (#157)

--- packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,13 +1,21 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+import * as ActivityBarItemFlags from '../ActivityBarItemFlags/ActivityBarItemFlags.ts'
 import { getActivityBarItems } from '../GetActivityBarItems/GetActivityBarItems.ts'
+import { setFlag } from '../SetFlag/SetFlag.ts'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 
 export const loadContent = async (state: ActivityBarState, savedState: any): Promise<ActivityBarState> => {
   const items = getActivityBarItems()
+  const explorerIndex = 0
+  const itemsWithSelected = items.map((item, index) => {
+    const isSelected = index === explorerIndex
+    return setFlag(item, ActivityBarItemFlags.Selected, isSelected)
+  })
   return {
     ...state,
-    activityBarItems: items,
+    activityBarItems: itemsWithSelected,
     sideBarVisible: true,
     currentViewletId: ViewletModuleId.Explorer,
+    selectedIndex: explorerIndex,
   }
 }
