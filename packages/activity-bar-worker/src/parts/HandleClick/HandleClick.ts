7f771dd2a2e71064cbafe04d154a2e64eb1f5065
Le Vivilet <levivilet@pm.me>
2025-10-27 10:43:39 +0100

    feature: move function

--- packages/activity-bar-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/activity-bar-worker/src/parts/HandleClick/HandleClick.ts
@@ -1,27 +1,9 @@
-import { MouseEventType } from '@lvce-editor/constants'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import { getIndexFromPosition } from '../GetIndexFromPosition/GetIndexFromPosition.ts'
-import * as HandleClickAdditionalViews from '../HandleClickAdditionalViews/HandleClickAdditionalViews.ts'
-import * as HandleClickOther from '../HandleClickOther/HandleClickOther.ts'
-import * as HandleClickSettings from '../HandleClickSettings/HandleClickSettings.ts'
+import { handleClickIndex } from '../HandleClickIndex/HandleClickIndex.ts'
 
 export const handleClick = async (state: ActivityBarState, button: number, x: number, y: number): Promise<ActivityBarState> => {
-  if (button !== MouseEventType.LeftClick) {
-    return state
-  }
   const { activityBarItems, itemHeight } = state
   const index = getIndexFromPosition(x, y, itemHeight, activityBarItems.length)
-  if (index === -1) {
-    return state
-  }
-  const item = activityBarItems[index]
-  const viewletId = item.id
-  switch (viewletId) {
-    case 'Settings':
-      return HandleClickSettings.handleClickSettings(state, x, y, viewletId)
-    case 'Additional Views':
-      return HandleClickAdditionalViews.handleClickAdditionalViews(state, x, y, viewletId)
-    default:
-      return HandleClickOther.handleClickOther(state, x, y, viewletId)
-  }
+  return handleClickIndex(state, button, index, x, y)
 }
