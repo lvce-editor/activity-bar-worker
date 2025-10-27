2847189e997582e4347df7b8fc93f1f487159b57
Le Vivilet <levivilet@pm.me>
2025-10-27 13:29:24 +0100

    feature: take into account sidebar visiblity when clicking activity bar item (#112)

--- packages/activity-bar-worker/src/parts/SideBar/SideBar.ts
+++ packages/activity-bar-worker/src/parts/SideBar/SideBar.ts
@@ -1,8 +1,12 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const show = async (id: string): Promise<void> => {
-  // @ts-ignore
-  await RendererWorker.invoke('Layout.showSideBar', /* id */ id)
+export const show = async (sideBarVisible: boolean, id: string): Promise<void> => {
+  if (sideBarVisible) {
+    await RendererWorker.invoke('SideBar.show', id)
+  } else {
+    // @ts-ignore
+    await RendererWorker.invoke('Layout.showSideBar', /* id */ id)
+  }
 }
 
 export const hide = async (): Promise<void> => {
