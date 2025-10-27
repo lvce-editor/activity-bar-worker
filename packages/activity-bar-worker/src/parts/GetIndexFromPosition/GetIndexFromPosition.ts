23b7c3dccacf5bfcb747e57229e1dd77efb4ce10
Le Vivilet <levivilet@pm.me>
2025-10-27 11:06:35 +0100

    feature: improve index computation

--- packages/activity-bar-worker/src/parts/GetIndexFromPosition/GetIndexFromPosition.ts
+++ packages/activity-bar-worker/src/parts/GetIndexFromPosition/GetIndexFromPosition.ts
@@ -1,5 +1,6 @@
-export const getIndexFromPosition = (eventX: number, eventY: number, itemHeight: number, itemCount: number): number => {
-  const index = Math.floor(eventY / itemHeight)
+export const getIndexFromPosition = (y: number, eventX: number, eventY: number, itemHeight: number, itemCount: number): number => {
+  const relativeY = eventY - y
+  const index = Math.floor(relativeY / itemHeight)
   if (index < 0 || index >= itemCount) {
     return -1
   }
--- packages/activity-bar-worker/src/parts/HandleClick/HandleClick.ts
+++ packages/activity-bar-worker/src/parts/HandleClick/HandleClick.ts
@@ -2,8 +2,8 @@ import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import { getIndexFromPosition } from '../GetIndexFromPosition/GetIndexFromPosition.ts'
 import { handleClickIndex } from '../HandleClickIndex/HandleClickIndex.ts'
 
-export const handleClick = async (state: ActivityBarState, button: number, x: number, y: number): Promise<ActivityBarState> => {
-  const { activityBarItems, itemHeight } = state
-  const index = getIndexFromPosition(x, y, itemHeight, activityBarItems.length)
-  return handleClickIndex(state, button, index, x, y)
+export const handleClick = async (state: ActivityBarState, button: number, eventX: number, eventY: number): Promise<ActivityBarState> => {
+  const { activityBarItems, itemHeight, y } = state
+  const index = getIndexFromPosition(y, eventX, eventY, itemHeight, activityBarItems.length)
+  return handleClickIndex(state, button, index, eventX, eventY)
 }
