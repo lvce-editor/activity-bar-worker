186bb336471df16192b8088d85b6ad78e206f89b
Le Vivilet <levivilet@pm.me>
2025-10-27 10:26:15 +0100

    feature: add events (#83)

--- packages/activity-bar-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/activity-bar-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -1,5 +1,26 @@
+import { EventExpression } from '@lvce-editor/constants'
 import type { DomEventListener } from '../DomEventListener/DomEventListener.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 export const renderEventListeners = (): readonly DomEventListener[] => {
-  return []
+  return [
+    {
+      name: DomEventListenerFunctions.HandleBlur,
+      params: ['handleBlur'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleFocus,
+      params: ['handleFocus'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleContextMenu,
+      params: ['handleContextMenu', EventExpression.Button, EventExpression.ClientX, EventExpression.ClientY],
+    },
+    {
+      name: DomEventListenerFunctions.HandleMouseDown,
+      params: ['handleClick', EventExpression.Button, EventExpression.ClientX, EventExpression.ClientY],
+      preventDefault: true,
+      stopPropagation: true,
+    },
+  ]
 }
