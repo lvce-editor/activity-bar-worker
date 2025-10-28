4757af70b17fa5e8048182b88e7b5df49769e2e6
Le Vivilet <levivilet@pm.me>
2025-10-28 22:35:42 +0100

    skip

--- packages/e2e/src/activity-bar.explorer.ts
+++ packages/e2e/src/activity-bar.explorer.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'activity-bar.explorer'
 
+export const skip = 1
+
 export const test: Test = async ({ Locator, expect }) => {
   // assert
   const explorer = Locator('.ActivityBarItem[title="Explorer"]')
