1d397c6c0a2ac94349cb0475261dea09217b7073
Le Vivilet <levivilet@pm.me>
2025-11-05 21:13:55 +0100

    feature: rerender when update state changes (#172)

--- packages/activity-bar-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/activity-bar-worker/src/parts/DiffItems/DiffItems.ts
@@ -1,5 +1,10 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 
 export const isEqual = (oldState: ActivityBarState, newState: ActivityBarState): boolean => {
-  return oldState.focusedIndex === newState.focusedIndex && oldState.activityBarItems === newState.activityBarItems
+  return (
+    oldState.focusedIndex === newState.focusedIndex &&
+    oldState.activityBarItems === newState.activityBarItems &&
+    oldState.updateProgress === newState.updateProgress &&
+    oldState.updateState === newState.updateState
+  )
 }
