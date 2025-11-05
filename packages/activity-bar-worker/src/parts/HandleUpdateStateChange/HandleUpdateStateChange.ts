132cbf76c24370eb1d1a8895e75b992790f4dfd9
Le Vivilet <levivilet@pm.me>
2025-11-05 23:08:42 +0100

    feature: set badgeText when update has been downloaded (#184)

--- packages/activity-bar-worker/src/parts/HandleUpdateStateChange/HandleUpdateStateChange.ts
+++ packages/activity-bar-worker/src/parts/HandleUpdateStateChange/HandleUpdateStateChange.ts
@@ -2,15 +2,26 @@ import type { ActivityBarItem } from '../ActivityBarItem/ActivityBarItem.ts'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import type { UpdateConfig } from '../UpdateConfig/UpdateConfig.ts'
 import * as ActivityBarItemFlags from '../ActivityBarItemFlags/ActivityBarItemFlags.ts'
+import * as UpdateState from '../UpdateState/UpdateState.ts'
 
 const getNewItems = (items: readonly ActivityBarItem[], state: number): readonly ActivityBarItem[] => {
   return items.map((item) => {
-    if (item.id === 'Settings' && state === 1) {
-      return {
-        ...item,
-        badgeIcon: 'clock',
-        flags: item.flags | ActivityBarItemFlags.Progress,
+    if (item.id === 'Settings') {
+      if (state === UpdateState.CheckingForUpdate) {
+        return {
+          ...item,
+          badgeIcon: 'clock',
+          flags: item.flags | ActivityBarItemFlags.Progress,
+        }
       }
+      if (state === UpdateState.WaitingForRestart) {
+        return {
+          ...item,
+          badgeIcon: '',
+          badgeText: '1',
+        }
+      }
+      return item
     }
     return item
   })
