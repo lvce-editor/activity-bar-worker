e61259733cddf62b4357310e57f08db22e2a1a99
Le Vivilet <levivilet@pm.me>
2025-11-05 23:06:03 +0100

    feature: render badge text

---
+++ packages/activity-bar-worker/src/parts/GetActivityBarItemWithBadgeDom/GetActivityBarItemWithBadgeDom.ts
@@ -0,0 +1,46 @@
+import { AriaRoles } from '@lvce-editor/constants'
+import { type VirtualDomNode, mergeClassNames, text, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { ActivityBarItem } from '../ActivityBarItem/ActivityBarItem.ts'
+import * as ActivityBarItemFlags from '../ActivityBarItemFlags/ActivityBarItemFlags.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import { getAriaSelected } from '../GetAriaSelected/GetAriaSelected.ts'
+import { getClassName } from '../GetClassName/GetClassName.ts'
+
+export const getActivityBarItemWithBadgeDom = (item: ActivityBarItem): readonly VirtualDomNode[] => {
+  const { flags, title, icon, badgeText } = item
+  if (!badgeText) {
+    // TODO should not happen
+    return []
+  }
+  const isTab = flags & ActivityBarItemFlags.Tab
+  const isSelected = flags & ActivityBarItemFlags.Selected
+  const isFocused = flags & ActivityBarItemFlags.Focused
+  const role = isTab ? AriaRoles.Tab : AriaRoles.Button
+  const ariaSelected = getAriaSelected(isTab, isSelected)
+  const marginTop = flags & ActivityBarItemFlags.MarginTop
+  let className = getClassName(isFocused, marginTop, isSelected)
+  className += ' ' + ClassNames.ActivityBarItemNested
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className,
+      ariaLabel: '',
+      title,
+      role,
+      ariaSelected,
+      childCount: 2,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: mergeClassNames(ClassNames.Icon, `MaskIcon${icon}`),
+      role: AriaRoles.None,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ActivityBarItemBadge,
+      childCount: 1,
+    },
+    text(badgeText),
+  ]
+}
