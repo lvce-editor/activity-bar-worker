edff62d061e69c7520354f53207378bdea43ab3f
Le Vivilet <levivilet@pm.me>
2025-11-05 21:15:50 +0100

    feature: update items when update state changes

--- packages/activity-bar-worker/src/parts/HandleUpdateStateChange/HandleUpdateStateChange.ts
+++ packages/activity-bar-worker/src/parts/HandleUpdateStateChange/HandleUpdateStateChange.ts
@@ -1,10 +1,26 @@
+import type { ActivityBarItem } from '../ActivityBarItem/ActivityBarItem.ts'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import type { UpdateConfig } from '../UpdateConfig/UpdateConfig.ts'
 
+const getNewItems = (items: readonly ActivityBarItem[], state: number): readonly ActivityBarItem[] => {
+  return items.map((item) => {
+    if (item.id === 'Settings' && state === 1) {
+      return {
+        ...item,
+        badgeIcon: 'clock',
+      }
+    }
+    return item
+  })
+}
+
 export const handleUpdateStateChange = async (state: ActivityBarState, config: UpdateConfig): Promise<ActivityBarState> => {
+  const { activityBarItems } = state
+  const newItems = getNewItems(activityBarItems, config.state)
   return {
     ...state,
     updateState: config.state,
     updateProgress: config.progress,
+    activityBarItems: newItems,
   }
 }
