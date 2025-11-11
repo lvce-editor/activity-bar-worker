08cc6f0ae02e8faf49b80fe4eada3b1d04257b79
Le Vivilet <levivilet@pm.me>
2025-11-11 22:23:04 +0100

    feature: set filtered items in loadContent

--- packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,17 +1,21 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import { getActivityBarItems } from '../GetActivityBarItems/GetActivityBarItems.ts'
+import { getFilteredActivityBarItems } from '../GetFilteredActivityBarItems/GetFilteredActivityBarItems.ts'
 import { markSelected } from '../MarkSelected/MarkSelected.ts'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 
 export const loadContent = async (state: ActivityBarState, savedState: any): Promise<ActivityBarState> => {
+  const { itemHeight, height } = state
   const items = getActivityBarItems()
   const explorerIndex = 0
   const itemsWithSelected = markSelected(items, explorerIndex)
+  const filteredItems = getFilteredActivityBarItems(itemsWithSelected, height, itemHeight)
   return {
     ...state,
     activityBarItems: itemsWithSelected,
-    sideBarVisible: true,
     currentViewletId: ViewletModuleId.Explorer,
+    filteredItems,
     selectedIndex: explorerIndex,
+    sideBarVisible: true,
   }
 }
