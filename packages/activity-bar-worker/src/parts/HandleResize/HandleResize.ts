e42e9f1773f458fd224119b06dae12da61bd11ca
Le Vivilet <levivilet@pm.me>
2025-11-11 22:04:58 +0100

    feature: set properties on resize

--- packages/activity-bar-worker/src/parts/HandleResize/HandleResize.ts
+++ packages/activity-bar-worker/src/parts/HandleResize/HandleResize.ts
@@ -1,9 +1,25 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 
-export const handleResize = (state: ActivityBarState): ActivityBarState => {
-  // TODO
-  // 1. update dimensions
-  // 2. update activity bar items, depending on available height
-  // 3. render ellipsis with additional items if needed
-  return state
+// TODO
+// 1. update dimensions
+// 2. update activity bar items, depending on available height
+// 3. render ellipsis with additional items if needed
+interface Dimensions {
+  readonly x: number
+  readonly y: number
+  readonly width: number
+  readonly height: number
+}
+
+export const handleResize = (state: ActivityBarState, dimensions: Dimensions): ActivityBarState => {
+  const { x, y, width, height } = dimensions
+
+  return {
+    ...state,
+    x,
+    y,
+    width,
+    // @ts-ignore
+    height,
+  }
 }
