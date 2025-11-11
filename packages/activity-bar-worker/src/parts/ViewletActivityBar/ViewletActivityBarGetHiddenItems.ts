b19fc26b254c705647c088922d9ed6e8668c2904
Le Vivilet <levivilet@pm.me>
2025-11-11 22:12:56 +0100

    feature add hidden items (#198)

---
+++ packages/activity-bar-worker/src/parts/ViewletActivityBar/ViewletActivityBarGetHiddenItems.ts
@@ -0,0 +1,17 @@
+import type { ActivityBarItem } from '../ActivityBarItem/ActivityBarItem.ts'
+import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+
+export const getNumberOfVisibleItems = (state: ActivityBarState): number => {
+  const { height, itemHeight } = state
+  const numberOfVisibleItemsTop = Math.floor(height / itemHeight)
+  return numberOfVisibleItemsTop
+}
+
+export const getHiddenItems = (state: ActivityBarState): readonly ActivityBarItem[] => {
+  const numberOfVisibleItems = getNumberOfVisibleItems(state)
+  const items = state.activityBarItems
+  if (numberOfVisibleItems >= items.length) {
+    return []
+  }
+  return state.activityBarItems.slice(numberOfVisibleItems - 2, -1)
+}
