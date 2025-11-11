06a3a8b8df7a8c17d2b1a4aa7f08a6649a7e9e55
Le Vivilet <levivilet@pm.me>
2025-11-11 21:55:54 +0100

    feature: add todo

--- packages/activity-bar-worker/src/parts/HandleResize/HandleResize.ts
+++ packages/activity-bar-worker/src/parts/HandleResize/HandleResize.ts
@@ -1,5 +1,9 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 
 export const handleResize = (state: ActivityBarState): ActivityBarState => {
+  // TODO
+  // 1. update dimensions
+  // 2. update activity bar items, depending on available height
+  // 3. render ellipsis with additional items if needed
   return state
 }
