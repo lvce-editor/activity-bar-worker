2e2aaeabfb9b38e009a6b6b2825dade82f75b895
Le Vivilet <levivilet@pm.me>
2025-11-12 21:18:11 +0100

    feature: update ids

--- packages/activity-bar-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/activity-bar-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -2,7 +2,7 @@ import { MenuEntryId } from '@lvce-editor/constants'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 
 export const getMenuEntryIds = (): readonly number[] => {
-  return [MenuEntryId.ActivityBar, MenuEntryId.ActivityBarAdditionalViews]
+  return [MenuEntryId.ActivityBar, MenuEntryId.ActivityBarAdditionalViews, MenuEntryId.Settings]
 }
 
 export const getMenuEntries = (): readonly MenuEntry[] => {
