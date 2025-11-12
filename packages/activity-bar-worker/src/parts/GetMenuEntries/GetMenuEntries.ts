a450f82d2184fe1111af0a75e62eeac763a82a09
Le Vivilet <levivilet@pm.me>
2025-11-12 22:01:17 +0100

    fix: menu entries (#229)

--- packages/activity-bar-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/activity-bar-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -1,17 +1,19 @@
 import { MenuEntryId } from '@lvce-editor/constants'
+import type { ContextMenuOptions } from '../ContextMenuOptions/ContextMenuOptions.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import * as ActivityBarStates from '../ActivityBarStates/ActivityBarStates.ts'
 import { getMenuEntriesActivityBar } from '../GetMenuEntriesActivityBar/GetMenuEntriesActivityBar.ts'
 import { getMenuEntriesAdditionalViews } from '../GetMenuEntriesAdditionalViews/GetMenuEntriesAdditionalViews.ts'
 import { getMenuEntriesSettings } from '../GetMenuEntriesSettings/GetMenuEntriesSettings.ts'
 
-export const getMenuEntries = (id: number): readonly MenuEntry[] => {
+export const getMenuEntries = (id: number, options: ContextMenuOptions): readonly MenuEntry[] => {
   const tuple = ActivityBarStates.get(id)
   if (!tuple) {
     return []
   }
   const { newState } = tuple
-  switch (id) {
+  const { menuId } = options
+  switch (menuId) {
     case MenuEntryId.ActivityBar:
       return getMenuEntriesActivityBar(newState)
     case MenuEntryId.ActivityBarAdditionalViews:
