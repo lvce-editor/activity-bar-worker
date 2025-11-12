bef4d6e2ca951f51b4605006ca46afac12649f92
Le Vivilet <levivilet@pm.me>
2025-11-12 22:00:05 +0100

    move

--- packages/activity-bar-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/activity-bar-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -1,13 +1,10 @@
 import { MenuEntryId } from '@lvce-editor/constants'
+import type { ContextMenuOptions } from '../ContextMenuOptions/ContextMenuOptions.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import * as ActivityBarStates from '../ActivityBarStates/ActivityBarStates.ts'
 import { getMenuEntriesActivityBar } from '../GetMenuEntriesActivityBar/GetMenuEntriesActivityBar.ts'
 import { getMenuEntriesAdditionalViews } from '../GetMenuEntriesAdditionalViews/GetMenuEntriesAdditionalViews.ts'
 import { getMenuEntriesSettings } from '../GetMenuEntriesSettings/GetMenuEntriesSettings.ts'
-
-interface ContextMenuOptions {
-  readonly menuId: number
-}
 
 export const getMenuEntries = (id: number, options: ContextMenuOptions): readonly MenuEntry[] => {
   const tuple = ActivityBarStates.get(id)
