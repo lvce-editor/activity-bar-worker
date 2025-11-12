004a6ac52c8081c6e53dd51edc40857b4803f94f
Le Vivilet <levivilet@pm.me>
2025-11-12 21:09:22 +0100

    feature: add menu entry ids

---
+++ packages/activity-bar-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -0,0 +1,11 @@
+import { MenuEntryId } from '@lvce-editor/constants'
+import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
+
+export const getMenuEntryIds = (): readonly number[] => {
+  return [MenuEntryId.ActivityBar, MenuEntryId.ActivityBarAdditionalViews]
+}
+
+export const getMenuEntries = (): readonly MenuEntry[] => {
+  // TODO
+  return []
+}
