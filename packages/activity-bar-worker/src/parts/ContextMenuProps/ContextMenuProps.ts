bda0f43c8d0431abe82774f439c7f6404e952446
Le Vivilet <levivilet@pm.me>
2025-11-26 16:54:44 +0100

    feature: add context menu types (#234)

---
+++ packages/activity-bar-worker/src/parts/ContextMenuProps/ContextMenuProps.ts
@@ -0,0 +1,20 @@
+import type { MenuEntryId } from '@lvce-editor/constants'
+
+export interface ContextMenuPropsBase {
+  readonly menuId: number
+}
+
+export interface ContextMenuPropsAdditionalViews extends ContextMenuPropsBase {
+  readonly menuId: typeof MenuEntryId.ActivityBarAdditionalViews
+  readonly viewletId: string
+}
+
+export interface ContextMenuPropsSettings extends ContextMenuPropsBase {
+  readonly menuId: typeof MenuEntryId.Settings
+}
+
+export interface ContextMenuPropsActivityBar extends ContextMenuPropsBase {
+  readonly menuId: typeof MenuEntryId.ActivityBar
+}
+
+export type ContextMenuProps = ContextMenuPropsAdditionalViews | ContextMenuPropsSettings | ContextMenuPropsActivityBar
