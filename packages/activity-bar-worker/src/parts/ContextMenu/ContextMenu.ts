1bd03f0e82c7033c2bae8ad2968a868fe868db9d
Le Vivilet <levivilet@pm.me>
2025-11-26 16:46:00 +0100

    feature: remove unused code

--- packages/activity-bar-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/activity-bar-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -1,9 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
-export const show = async (x: number, y: number, id: number, ...args: readonly any[]): Promise<void> => {
-  await RendererWorker.showContextMenu(x, y, id, args)
-}
-
 export const show2 = async (uid: number, menuId: any, x: number, y: number, args: any): Promise<void> => {
   // @ts-ignore
   await RendererWorker.invoke('ContextMenu.show2', uid, menuId, x, y, args)
