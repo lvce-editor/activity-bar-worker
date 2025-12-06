cf59cc6b8880c0e5ab081d0295d72cc56fd4327b
Le Vivilet <levivilet@pm.me>
2025-12-06 11:31:44 +0100

    feature: query activity bar badges on start

--- packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -3,6 +3,7 @@ import { getActivityBarItems } from '../GetActivityBarItems/GetActivityBarItems.
 import { getFilteredActivityBarItems } from '../GetFilteredActivityBarItems/GetFilteredActivityBarItems.ts'
 import { markSelected } from '../MarkSelected/MarkSelected.ts'
 import * as SideBarLocationType from '../SideBarLocationType/SideBarLocationType.ts'
+import { updateItemsWithBadgeCount } from '../UpdateItemsWithBadgeCount/UpdateItemsWithBadgeCount.ts'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 
 export const loadContent = async (state: ActivityBarState, savedState: any): Promise<ActivityBarState> => {
@@ -11,11 +12,12 @@ export const loadContent = async (state: ActivityBarState, savedState: any): Pro
   const explorerIndex = 0
   const itemsWithSelected = markSelected(items, explorerIndex)
   const filteredItems = getFilteredActivityBarItems(itemsWithSelected, height, itemHeight)
+  const newItems = await updateItemsWithBadgeCount(filteredItems)
   return {
     ...state,
     activityBarItems: itemsWithSelected,
     currentViewletId: ViewletModuleId.Explorer,
-    filteredItems,
+    filteredItems: newItems,
     selectedIndex: explorerIndex,
     sideBarLocation: SideBarLocationType.Left,
     sideBarVisible: true,
