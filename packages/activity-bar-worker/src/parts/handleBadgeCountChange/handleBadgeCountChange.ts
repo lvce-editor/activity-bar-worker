bbae782e8a40d754607f44a26bff6cdb70b09cd9
Le Vivilet <levivilet@pm.me>
2025-12-06 11:25:18 +0100

    feature: display source control badge in activity bar (#248)

--- packages/activity-bar-worker/src/parts/handleBadgeCountChange/handleBadgeCountChange.ts
+++ packages/activity-bar-worker/src/parts/handleBadgeCountChange/handleBadgeCountChange.ts
@@ -1,6 +1,19 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 
 export const handleBadgeCountChange = async (state: ActivityBarState): Promise<ActivityBarState> => {
-  // TODO
-  return state
+  const { filteredItems } = state
+  const badgeCounts = await RendererWorker.invoke('Layout.getBadgeCounts')
+  const newItems = filteredItems.map((item) => {
+    const badgeCount = badgeCounts[item.id] || 0
+    const badgeText = badgeCount ? `${badgeCount}` : ''
+    return {
+      ...item,
+      badgeText,
+    }
+  })
+  return {
+    ...state,
+    filteredItems: newItems,
+  }
 }
