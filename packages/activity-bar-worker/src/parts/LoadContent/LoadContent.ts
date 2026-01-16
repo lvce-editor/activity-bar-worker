00a64d7fc8255a4b3d693436b235742d1e5efc01
Le Vivilet <levivilet@pm.me>
2026-01-16 02:08:35 +0100

    feature: ask active viewlet id from renderer worker (#262)

--- packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,3 +1,4 @@
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import { getActivityBarItems } from '../GetActivityBarItems/GetActivityBarItems.ts'
 import { getFilteredActivityBarItems } from '../GetFilteredActivityBarItems/GetFilteredActivityBarItems.ts'
@@ -6,11 +7,21 @@ import * as SideBarLocationType from '../SideBarLocationType/SideBarLocationType
 import { updateItemsWithBadgeCount } from '../UpdateItemsWithBadgeCount/UpdateItemsWithBadgeCount.ts'
 import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 
+export const getActiveView = async (): Promise<string> => {
+  try {
+    const activeView = await RendererWorker.invoke('Layout.getActiveSideBarView')
+    return activeView
+  } catch {
+    return ViewletModuleId.Explorer
+  }
+}
+
 export const loadContent = async (state: ActivityBarState, savedState: any): Promise<ActivityBarState> => {
   const { height, itemHeight } = state
   const items = getActivityBarItems()
-  const explorerIndex = 0
-  const itemsWithSelected = markSelected(items, explorerIndex)
+  const activeView = await getActiveView()
+  const index = items.findIndex((item) => item.id === activeView)
+  const itemsWithSelected = markSelected(items, index)
   const filteredItems = getFilteredActivityBarItems(itemsWithSelected, height, itemHeight)
   const newItems = await updateItemsWithBadgeCount(filteredItems)
   return {
@@ -18,7 +29,7 @@ export const loadContent = async (state: ActivityBarState, savedState: any): Pro
     activityBarItems: itemsWithSelected,
     currentViewletId: ViewletModuleId.Explorer,
     filteredItems: newItems,
-    selectedIndex: explorerIndex,
+    selectedIndex: index,
     sideBarLocation: SideBarLocationType.Left,
     sideBarVisible: true,
   }
