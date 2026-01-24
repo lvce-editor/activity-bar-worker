7ab3488b503555bd0072e7097a82646c6596b6bb
Le Vivilet <levivilet@pm.me>
2026-01-24 23:49:52 +0100

    feature: add account string (#268)

--- packages/activity-bar-worker/src/parts/ActivityBarStrings/ActivityBarStrings.ts
+++ packages/activity-bar-worker/src/parts/ActivityBarStrings/ActivityBarStrings.ts
@@ -1,6 +1,10 @@
 import * as I18nString from '../I18NString/I18NString.ts'
 import * as UiStrings from '../UiStrings/UiStrings.ts'
 
+export const account = (): string => {
+  return I18nString.i18nString(UiStrings.Account)
+}
+
 export const explorer = (): string => {
   return I18nString.i18nString(UiStrings.Explorer)
 }
