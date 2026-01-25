bf2ff93c51191e98260104ed79887ca2d538ac9c
Le Vivilet <levivilet@pm.me>
2026-01-25 15:32:20 +0100

    feature: update activity bar items when toggling accountEnabled (#285)

--- packages/activity-bar-worker/src/parts/SetAccountEnabled/SetAccountEnabled.ts
+++ packages/activity-bar-worker/src/parts/SetAccountEnabled/SetAccountEnabled.ts
@@ -1,8 +1,22 @@
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
+import { getActivityBarItems } from '../GetActivityBarItems/GetActivityBarItems.ts'
+import { getFilteredActivityBarItems } from '../GetFilteredActivityBarItems/GetFilteredActivityBarItems.ts'
+import { markSelected } from '../MarkSelected/MarkSelected.ts'
 
 export const setAccountEnabled = (state: ActivityBarState, enabled: boolean): ActivityBarState => {
-  return {
+  const { height, itemHeight, selectedIndex } = state
+  const newState = {
     ...state,
     accountEnabled: enabled,
+  }
+
+  const newActivityBarItems = getActivityBarItems(newState)
+  const markedItems = markSelected(newActivityBarItems, selectedIndex)
+  const filteredItems = getFilteredActivityBarItems(markedItems, height, itemHeight)
+
+  return {
+    ...newState,
+    activityBarItems: markedItems,
+    filteredItems: filteredItems,
   }
 }
