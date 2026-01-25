db4b9a6df609e6844530bb386f9b2f8c08384a9e
Le Vivilet <levivilet@pm.me>
2026-01-25 11:40:10 +0100

    feature: don't save uid property

--- packages/activity-bar-worker/src/parts/SaveState/SaveState.ts
+++ packages/activity-bar-worker/src/parts/SaveState/SaveState.ts
@@ -2,9 +2,8 @@ import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import type { SavedState } from '../SavedState/SavedState.ts'
 
 export const saveState = (state: ActivityBarState): SavedState => {
-  const { currentViewletId, uid } = state
+  const { currentViewletId } = state
   return {
     currentViewletId,
-    uid,
   }
 }
--- packages/activity-bar-worker/test/SaveState.test.ts
+++ packages/activity-bar-worker/test/SaveState.test.ts
@@ -1,77 +1,31 @@
 import { expect, test } from '@jest/globals'
-import type { ActivityBarItem } from '../src/parts/ActivityBarItem/ActivityBarItem.ts'
 import type { ActivityBarState } from '../src/parts/ActivityBarState/ActivityBarState.ts'
 import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import { saveState } from '../src/parts/SaveState/SaveState.ts'
 
-test('saveState returns SavedState with uid and currentViewletId', () => {
+test('saveState returns SavedState with currentViewletId', () => {
   const state: ActivityBarState = {
     ...createDefaultState(),
     currentViewletId: 'explorer',
-    uid: 123,
   }
 
   const result = saveState(state)
 
-  expect(result).toEqual({ currentViewletId: 'explorer', uid: 123 })
+  expect(result).toEqual({ currentViewletId: 'explorer' })
 })
 
-test('saveState preserves uid and currentViewletId from state', () => {
-  const items: readonly ActivityBarItem[] = [
-    {
-      flags: 0,
-      icon: 'icon1',
-      id: 'item1',
-      keyShortcuts: '',
-      title: 'Item 1',
-    },
-    {
-      flags: 0,
-      icon: 'icon2',
-      id: 'item2',
-      keyShortcuts: '',
-      title: 'Item 2',
-    },
-  ]
-
+test('saveState preserves currentViewletId from state', () => {
   const state: ActivityBarState = {
     ...createDefaultState(),
-    activityBarItems: items,
     currentViewletId: 'search',
     focused: true,
     focusedIndex: 2,
-    uid: 456,
   }
 
   const result = saveState(state)
 
-  expect(result.uid).toBe(456)
   expect(result.currentViewletId).toBe('search')
-  expect(result).toEqual({ currentViewletId: 'search', uid: 456 })
-})
-
-test('saveState handles uid of 0', () => {
-  const state: ActivityBarState = {
-    ...createDefaultState(),
-    currentViewletId: 'debug',
-    uid: 0,
-  }
-
-  const result = saveState(state)
-
-  expect(result).toEqual({ currentViewletId: 'debug', uid: 0 })
-})
-
-test('saveState handles negative uid', () => {
-  const state: ActivityBarState = {
-    ...createDefaultState(),
-    currentViewletId: 'scm',
-    uid: -1,
-  }
-
-  const result = saveState(state)
-
-  expect(result).toEqual({ currentViewletId: 'scm', uid: -1 })
+  expect(result).toEqual({ currentViewletId: 'search' })
 })
 
 test('saveState returns new object', () => {
