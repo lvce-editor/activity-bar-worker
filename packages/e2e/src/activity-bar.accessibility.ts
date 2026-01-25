28178cc074b58d837f6dd4ea959528e4de50c764
Le Vivilet <levivilet@pm.me>
2026-01-25 17:58:44 +0100

    skip

--- packages/e2e/src/activity-bar.accessibility.ts
+++ packages/e2e/src/activity-bar.accessibility.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'activity-bar.accessibility'
 
-export const skip = 0
+export const skip = 1
 
 export const test: Test = async ({ expect, Locator }) => {
   // Test activity bar accessibility attributes
