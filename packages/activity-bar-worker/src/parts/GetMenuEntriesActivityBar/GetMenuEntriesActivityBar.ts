cfd1cb71be0651847e578f0dc406c5bd0941336c
Le Vivilet <levivilet@pm.me>
2026-01-25 15:23:37 +0100

    feature: move toContextMenuItem into separate file

--- packages/activity-bar-worker/src/parts/GetMenuEntriesActivityBar/GetMenuEntriesActivityBar.ts
+++ packages/activity-bar-worker/src/parts/GetMenuEntriesActivityBar/GetMenuEntriesActivityBar.ts
@@ -1,21 +1,10 @@
 import { MenuItemFlags } from '@lvce-editor/constants'
-import type { ActivityBarItem } from '../ActivityBarItem/ActivityBarItem.ts'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
-import * as ActivityBarItemFlags from '../ActivityBarItemFlags/ActivityBarItemFlags.ts'
 import * as ViewletActivityBarStrings from '../ActivityBarStrings/ActivityBarStrings.ts'
 import { menuEntryMoveSideBar } from '../MenuEntryMoveSideBar/MenuEntryMoveSideBar.ts'
 import * as MenuEntrySeparator from '../MenuEntrySeparator/MenuEntrySeparator.ts'
-
-const toContextMenuItem = (activityBarItem: ActivityBarItem): MenuEntry => {
-  const isEnabled = activityBarItem.flags & ActivityBarItemFlags.Enabled
-  return {
-    command: '',
-    flags: isEnabled ? MenuItemFlags.Checked : MenuItemFlags.Unchecked,
-    id: '', // TODO
-    label: activityBarItem.id,
-  }
-}
+import { toContextMenuItem } from '../ToContextMenuItem/ToContextMenuItem.ts'
 
 export const getMenuEntriesActivityBar = (state: ActivityBarState): readonly MenuEntry[] => {
   const { activityBarItems, sideBarLocation } = state
