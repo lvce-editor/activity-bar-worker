6ee6312db4097a6e5c6cd9a1bfc23b7e846e2a78
Le Vivilet <levivilet@pm.me>
2026-02-19 00:12:12 +0100

    feature: optimize loadContent function and add test for sidebar visibility (#315)

--- packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/activity-bar-worker/src/parts/LoadContent/LoadContent.ts
@@ -11,22 +11,19 @@ import * as ViewletModuleId from '../ViewletModuleId/ViewletModuleId.ts'
 export const loadContent = async (state: ActivityBarState): Promise<ActivityBarState> => {
   const { height, itemHeight } = state
   const items = getActivityBarItems(state)
-  // TODO parallelize async calls
-  // or add one function that returns all needed data
-  const activeView = await getActiveView()
-  const sideBarVisible = await getSideBarVisible()
+  const [activeView, sideBarVisible, sidebarLocation] = await Promise.all([getActiveView(), getSideBarVisible(), getSideBarPosition()])
   const index = items.findIndex((item) => item.id === activeView)
-  const itemsWithSelected = markSelected(items, index)
+  const selectedIndex = sideBarVisible ? index : -1
+  const itemsWithSelected = markSelected(items, selectedIndex)
   const filteredItems = getFilteredActivityBarItems(itemsWithSelected, height, itemHeight)
   const newItems = await updateItemsWithBadgeCount(filteredItems)
-  const sidebarLocation = await getSideBarPosition()
   return {
     ...state,
     activityBarItems: itemsWithSelected,
     currentViewletId: ViewletModuleId.Explorer,
     filteredItems: newItems,
     initial: false,
-    selectedIndex: index,
+    selectedIndex,
     sideBarLocation: sidebarLocation,
     sideBarVisible,
   }
