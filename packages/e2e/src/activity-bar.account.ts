307a41120ba76ce6b4b5325f2cf59675071f2cdd
Le Vivilet <levivilet@pm.me>
2026-02-19 00:22:42 +0100

    skip

--- packages/e2e/src/activity-bar.account.ts
+++ packages/e2e/src/activity-bar.account.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'activity-bar.account'
 
+export const skip = 1
+
 export const test: Test = async ({ ActivityBar, expect, Locator }) => {
   // act
   await ActivityBar.setAccountEnabled(true)
