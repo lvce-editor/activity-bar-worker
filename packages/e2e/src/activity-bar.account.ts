b14d97a2111c03ed39cafc4d6602dfe7e5c6dbad
Le Vivilet <levivilet@pm.me>
2026-02-19 00:23:56 +0100

    refactor: replace Command with ActivityBar in account context menu tests (#317)

--- packages/e2e/src/activity-bar.account.ts
+++ packages/e2e/src/activity-bar.account.ts
@@ -2,9 +2,11 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'activity-bar.account'
 
-export const test: Test = async ({ Command, expect, Locator }) => {
+export const skip = 1
+
+export const test: Test = async ({ ActivityBar, expect, Locator }) => {
   // act
-  await Command.execute('ActivityBar.setAccountEnabled', true)
+  await ActivityBar.setAccountEnabled(true)
 
   // assert
   const account = Locator('.ActivityBarItem[title="Account"]')
