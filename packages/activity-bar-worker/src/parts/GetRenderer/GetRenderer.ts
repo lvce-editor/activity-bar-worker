14a1b07b7b814d1763a420b0a892399b85ef7339
Le Vivilet <levivilet@pm.me>
2026-02-19 01:28:38 +0100

    refactor: remove unused RenderItems from DiffType and related tests

--- packages/activity-bar-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/activity-bar-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -1,9 +1,8 @@
-import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import { renderCss } from '../RenderCss/RenderCss.ts'
+import type { Renderer } from '../Renderer/Renderer.ts'
 import * as RenderFocusContext from '../RenderFocusContext/RenderFocusContext.ts'
 import { renderIncremental } from '../RenderIncremental/RenderIncremental.ts'
-import * as RenderItems from '../RenderItems/RenderItems.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
@@ -15,8 +14,6 @@ export const getRenderer = (diffType: number): Renderer => {
       return RenderFocusContext.renderFocusContext
     case DiffType.RenderIncremental:
       return renderIncremental
-    case DiffType.RenderItems:
-      return RenderItems.renderItems
     default:
       throw new Error('unknown renderer')
   }
