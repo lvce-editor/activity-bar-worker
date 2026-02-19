56764abe0a6e44acf92f537506f480a17dc9552f
Le Vivilet <levivilet@pm.me>
2026-02-19 01:01:36 +0100

    fix: correct import path for ViewletRegistry in ActivityBarStates

--- packages/activity-bar-worker/src/parts/ActivityBarStates/ActivityBarStates.ts
+++ packages/activity-bar-worker/src/parts/ActivityBarStates/ActivityBarStates.ts
@@ -1,4 +1,4 @@
-import * as ViewletRegistry from '@lvce-editor//viewlet-registry'
+import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 
 export const { diff, get, getCommandIds, registerCommands, set, wrapCommand, wrapGetter } = ViewletRegistry.create<ActivityBarState>()
