fa80989614081aee0f7deafc7aa72ccfce4b67a2
Le Vivilet <levivilet@pm.me>
2026-04-06 17:55:25 +0200

    fix

--- packages/activity-bar-worker/test/CommandMap.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@jest/globals'
-import { commandMap } from '../src/parts/CommandMap/CommandMap.ts'
-
-test('commandMap registers account sign-in and sign-out commands', () => {
-  expect(commandMap['ActivityBar.handleClickSignIn']).toBeDefined()
-  expect(commandMap['ActivityBar.handleClickSignOut']).toBeDefined()
-})
