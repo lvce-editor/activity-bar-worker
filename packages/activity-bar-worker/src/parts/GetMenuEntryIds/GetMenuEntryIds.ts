0de8a02d90cd85b4c06bdda901278be06b7e8b52
Le Vivilet <levivilet@pm.me>
2026-04-06 21:12:47 +0200

    fix

--- packages/activity-bar-worker/src/parts/GetMenuEntryIds/GetMenuEntryIds.ts
+++ packages/activity-bar-worker/src/parts/GetMenuEntryIds/GetMenuEntryIds.ts
@@ -1,5 +1,6 @@
 import { MenuEntryId } from '@lvce-editor/constants'
+import { ACCOUNT_MENU_ID } from '../HandleClickAccount/HandleClickAccount.ts'
 
 export const getMenuEntryIds = (): readonly number[] => {
-  return [MenuEntryId.ActivityBar, MenuEntryId.ActivityBarAdditionalViews, MenuEntryId.Settings]
+  return [MenuEntryId.ActivityBar, MenuEntryId.ActivityBarAdditionalViews, MenuEntryId.Settings, ACCOUNT_MENU_ID]
 }
