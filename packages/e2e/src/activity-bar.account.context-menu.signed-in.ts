fcc5d84dd159f066f089397ee4f970b8412cd65f
Le Vivilet <levivilet@pm.me>
2026-04-06 21:17:11 +0200

    fix

--- packages/e2e/src/activity-bar.account.context-menu.signed-in.ts
+++ packages/e2e/src/activity-bar.account.context-menu.signed-in.ts
@@ -3,8 +3,12 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 export const name = 'activity-bar.account.context-menu.signed-in'
 
 export const test: Test = async ({ ActivityBar, expect, Locator }) => {
+  const activityBar = ActivityBar as typeof ActivityBar & {
+    setUserLoginState(state: 'logged in'): Promise<void>
+  }
+
   await ActivityBar.setAccountEnabled(true)
-  await (ActivityBar as any).setUserLoginState('logged in')
+  await activityBar.setUserLoginState('logged in')
 
   const account = Locator('.ActivityBarItem[title="Account"]')
   await account.click({ button: 'left' })
