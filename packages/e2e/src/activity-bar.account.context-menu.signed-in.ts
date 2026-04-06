8eeb27d04671dc138a3f64006b968aad81dee74c
Le Vivilet <levivilet@pm.me>
2026-04-06 22:04:16 +0200

    e2e

--- packages/e2e/src/activity-bar.account.context-menu.signed-in.ts
+++ packages/e2e/src/activity-bar.account.context-menu.signed-in.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'activity-bar.account.context-menu.signed-in'
 
+export const skip = 1
+
 export const test: Test = async ({ ActivityBar, expect, Locator }) => {
   const activityBar = ActivityBar as typeof ActivityBar & {
     setUserLoginState(state: 'logged in'): Promise<void>
