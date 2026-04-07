f862c82bd24cbc175bd099350aed2247d395ff1b
Le Vivilet <levivilet@pm.me>
2026-04-07 09:15:20 +0200

    feat: implement handleClickSignIn function to manage sign-in logic and update related test case

--- packages/activity-bar-worker/src/parts/HandleClickSignIn/HandleClickSignIn.ts
+++ packages/activity-bar-worker/src/parts/HandleClickSignIn/HandleClickSignIn.ts
@@ -1,5 +1,12 @@
+import { AuthWorker, RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 
 export const handleClickSignIn = async (state: ActivityBarState): Promise<ActivityBarState> => {
+  const { platform } = state
+  const backendUrl = await RendererWorker.invoke('Layout.getBackendUrl')
+  await AuthWorker.invoke('Auth.login', {
+    backendUrl,
+    platform,
+  })
   return state
 }
