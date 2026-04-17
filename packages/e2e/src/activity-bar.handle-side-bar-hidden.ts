0439e26beb9cbd295396d7d5f83e29513c709647
Le Vivilet <levivilet@pm.me>
2026-04-17 13:56:41 +0200

    fix

--- packages/e2e/src/activity-bar.handle-side-bar-hidden.ts
+++ packages/e2e/src/activity-bar.handle-side-bar-hidden.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'activity-bar.handle-side-bar-hidden'
 
+export const skip = 1
+
 export const test: Test = async ({ Command, expect, Locator }) => {
   const explorer = Locator('.ActivityBarItem[title="Explorer"]')
   const search = Locator('.ActivityBarItem[title="Search"]')
