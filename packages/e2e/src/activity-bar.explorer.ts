f613346d7f2bbb73b0ab4578dc49125e3a2801ad
Le Vivilet <levivilet@pm.me>
2026-04-29 23:03:06 +0200

    refactor: remove unused skip export in activity-bar.explorer test (#376)

--- packages/e2e/src/activity-bar.explorer.ts
+++ packages/e2e/src/activity-bar.explorer.ts
@@ -2,10 +2,9 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'activity-bar.explorer'
 
-export const skip = 1
-
-export const test: Test = async ({ expect, Locator }) => {
+export const test: Test = async ({ Command, expect, Locator }) => {
   // assert
+  await Command.execute('Layout.hideSideBar')
   const explorer = Locator('.ActivityBarItem[title="Explorer"]')
   await expect(explorer).toBeVisible()
   await expect(explorer).toHaveClass('IconFiles')
