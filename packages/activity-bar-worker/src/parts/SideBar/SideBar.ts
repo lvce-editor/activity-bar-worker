aeb147ba049d748740c527f8b43f71485768dfa0
Le Vivilet <levivilet@pm.me>
2026-05-05 08:50:10 +0000

    sidebar

--- packages/activity-bar-worker/src/parts/SideBar/SideBar.ts
+++ packages/activity-bar-worker/src/parts/SideBar/SideBar.ts
@@ -1,13 +1,21 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
+const showSideBarView = async (id: string): Promise<void> => {
+  await RendererWorker.invoke('SideBar.show', id)
+}
+
+const showHiddenSideBarView = async (id: string): Promise<void> => {
+  await showSideBarView(id)
+  // @ts-ignore
+  await RendererWorker.invoke('Layout.showSideBar', /* id */ id)
+}
+
 export const show = async (sideBarVisible: boolean, id: string): Promise<void> => {
   if (sideBarVisible) {
-    await RendererWorker.invoke('SideBar.show', id)
-  } else {
-    await RendererWorker.invoke('SideBar.show', id)
-    // @ts-ignore
-    await RendererWorker.invoke('Layout.showSideBar', /* id */ id)
+    await showSideBarView(id)
+    return
   }
+  await showHiddenSideBarView(id)
 }
 
 export const hide = async (): Promise<void> => {
