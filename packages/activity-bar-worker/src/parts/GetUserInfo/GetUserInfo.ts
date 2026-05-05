aef836025aa1d6400c1861822ca5130f6983f227
Le Vivilet <levivilet@pm.me>
2026-05-05 08:56:37 +0000

    fix

--- packages/activity-bar-worker/src/parts/GetUserInfo/GetUserInfo.ts
+++ packages/activity-bar-worker/src/parts/GetUserInfo/GetUserInfo.ts
@@ -6,12 +6,4 @@ export const getUserInfo = async (): Promise<unknown> => {
   } catch {
     return undefined
   }
-}import { RendererWorker } from '@lvce-editor/rpc-registry'
-
-export const getUserInfo = async (): Promise<unknown> => {
-  try {
-    return await RendererWorker.invoke('Layout.getUserInfo')
-  } catch {
-    return undefined
-  }
 }
--- packages/activity-bar-worker/src/parts/ToUserLoginState/ToUserLoginState.ts
+++ packages/activity-bar-worker/src/parts/ToUserLoginState/ToUserLoginState.ts
@@ -11,17 +11,4 @@ export const toUserLoginState = (userState: unknown): ActivityBarState['userLogi
     default:
       return 'logged out'
   }
-}import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
-
-export const toUserLoginState = (userState: unknown): ActivityBarState['userLoginState'] => {
-  switch (userState) {
-    case 'loggedIn':
-      return 'logged in'
-    case 'loggingIn':
-      return 'logging in'
-    case 'loggingOut':
-      return 'logging out'
-    default:
-      return 'logged out'
-  }
 }
