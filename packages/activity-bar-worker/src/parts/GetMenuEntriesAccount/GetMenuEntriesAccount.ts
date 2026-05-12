05add5dd833ba17c818b731ef7e529a6ef7003e9
Le Vivilet <levivilet@pm.me>
2026-05-12 22:51:58 +0200

    refactor: add TODO comment for potential layout query in getMenuEntriesAccount

--- packages/activity-bar-worker/src/parts/GetMenuEntriesAccount/GetMenuEntriesAccount.ts
+++ packages/activity-bar-worker/src/parts/GetMenuEntriesAccount/GetMenuEntriesAccount.ts
@@ -3,6 +3,7 @@ import type { ActivityBarState } from '../ActivityBarState/ActivityBarState.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 
 export const getMenuEntriesAccount = (state: ActivityBarState): readonly MenuEntry[] => {
+  // TODO maybe query it now from layout?
   const { userLoginState } = state
   const signInLabel = userLoginState === 'logging in' ? 'Signing In...' : 'Sign In'
   const signOutLabel = userLoginState === 'logging out' ? 'Signing Out...' : 'Sign Out'
