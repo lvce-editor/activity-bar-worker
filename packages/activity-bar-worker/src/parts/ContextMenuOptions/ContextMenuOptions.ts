10416767fa6ec143424f362ea0b7dcf5e44f70ff
Le Vivilet <levivilet@pm.me>
2026-06-26 17:55:29 +0200

    Fix knip unused file issues

--- packages/activity-bar-worker/src/parts/ContextMenuOptions/ContextMenuOptions.ts
+++
@@ -1,3 +0,0 @@
-export interface ContextMenuOptions {
-  readonly menuId: number
-}
--- packages/activity-bar-worker/src/parts/VisibleItem/VisibleItem.ts
+++
@@ -1,9 +0,0 @@
-export interface VisibleItem {
-  readonly hasMarginTop: boolean
-  readonly icon: string
-  readonly isFocused: boolean
-  readonly isProgress: boolean
-  readonly isSelected: boolean
-  readonly isTab: boolean
-  readonly title: string
-}
