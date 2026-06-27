0d81954cc377db6d679353274a401555aadfd0b2
Le Vivilet <levivilet@pm.me>
2026-06-27 11:45:03 +0200

    worker

--- packages/activity-bar-worker/src/parts/Listen/Listen.ts
+++ packages/activity-bar-worker/src/parts/Listen/Listen.ts
@@ -1,9 +1,10 @@
 import { registerCommands } from '../ActivityBarStates/ActivityBarStates.ts'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
 import { initializeAuthWorker } from '../InitializeAuthWorker/InitializeAuthWorker.ts'
+import { initializeExtensionManagementWorker } from '../InitializeExtensionManagementWorker/InitializeExtensionManagementWorker.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
   registerCommands(CommandMap.commandMap)
-  await Promise.all([initializeRendererWorker(), initializeAuthWorker()])
+  await Promise.all([initializeRendererWorker(), initializeAuthWorker(), initializeExtensionManagementWorker()])
 }
