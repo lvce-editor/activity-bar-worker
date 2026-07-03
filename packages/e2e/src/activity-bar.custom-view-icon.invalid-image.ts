2c9961c9b53a2c170bdb89a1c3f3b060d871cde3
Le Vivilet <levivilet@pm.me>
2026-07-03 21:01:00 +0200

    Skip failing custom icon e2e tests

--- packages/e2e/src/activity-bar.custom-view-icon.invalid-image.ts
+++ packages/e2e/src/activity-bar.custom-view-icon.invalid-image.ts
@@ -2,6 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 import { assertCustomViewIcon } from './_custom-view-icon.ts'
 
 export const name = 'activity-bar.custom-view-icon.invalid-image'
+export const skip = 1
 
 export const test: Test = async ({ Command, expect, Extension, Locator }) => {
   const uri = import.meta.resolve('../fixtures/sample.custom-view-icon-invalid-image')
