7f24f00fc637968cd7fea3fcce3e5f5de4739c9d
Le Vivilet <levivilet@pm.me>
2026-07-13 12:58:28 +0000

    fix: render lvce extension view icons

--- packages/activity-bar-worker/src/parts/IsCustomIconUrl/IsCustomIconUrl.ts
+++ packages/activity-bar-worker/src/parts/IsCustomIconUrl/IsCustomIconUrl.ts
@@ -1,3 +1,3 @@
 export const isCustomIconUrl = (icon: string): boolean => {
-  return icon.startsWith('http://') || icon.startsWith('https://') || icon.startsWith('file://') || icon.startsWith('/')
+  return icon.startsWith('http://') || icon.startsWith('https://') || icon.startsWith('file://') || icon.startsWith('lvce://') || icon.startsWith('/')
 }
--- packages/activity-bar-worker/test/IsCustomIconUrl.test.ts
+++ packages/activity-bar-worker/test/IsCustomIconUrl.test.ts
@@ -6,6 +6,7 @@ test('isCustomIconUrl detects url-like icons', () => {
   // eslint-disable-next-line unicorn/prefer-https
   expect(isCustomIconUrl('http://example.com/icon.svg')).toBe(true)
   expect(isCustomIconUrl('file:///tmp/icon.svg')).toBe(true)
+  expect(isCustomIconUrl('lvce://-/remote/home/test/.local/share/lvce/extensions/example/icon.svg')).toBe(true)
   expect(isCustomIconUrl('/icons/icon.svg')).toBe(true)
   expect(isCustomIconUrl('Extensions')).toBe(false)
   expect(isCustomIconUrl('symbol-beaker')).toBe(false)
