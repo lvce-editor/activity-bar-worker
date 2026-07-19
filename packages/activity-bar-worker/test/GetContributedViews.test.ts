ae279e051d3a01c01222c23fe3549ace3ef152d7
Le Vivilet <levivilet@pm.me>
2026-07-19 20:58:46 +0000

    fix: hoist static virtual dom nodes

--- packages/activity-bar-worker/test/GetContributedViews.test.ts
+++ packages/activity-bar-worker/test/GetContributedViews.test.ts
@@ -2,22 +2,24 @@ import { expect, test } from '@jest/globals'
 import { ExtensionManagementWorker } from '@lvce-editor/rpc-registry'
 import { getContributedViews } from '../src/parts/GetContributedViews/GetContributedViews.ts'
 
+const contributedViews = [
+  {
+    icon: '',
+    id: 'builtin.media-preview',
+    title: 'Media Preview',
+    type: 'preview',
+  },
+  {
+    icon: 'symbol-beaker',
+    id: 'sample.views.testing',
+    title: 'Testing',
+  },
+]
+
 test('getContributedViews excludes document preview views', async () => {
   using mockRpc = ExtensionManagementWorker.registerMockRpc({
     'Extensions.getViews'() {
-      return [
-        {
-          icon: '',
-          id: 'builtin.media-preview',
-          title: 'Media Preview',
-          type: 'preview',
-        },
-        {
-          icon: 'symbol-beaker',
-          id: 'sample.views.testing',
-          title: 'Testing',
-        },
-      ]
+      return contributedViews
     },
   })
 
