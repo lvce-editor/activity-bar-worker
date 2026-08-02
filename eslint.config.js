1113d923401b05a3fbe8c90de52a746f0858c43a
Le Vivilet <levivilet@pm.me>
2026-08-01 19:31:56 -0700

    feature: update eslint dependencies (#488)

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
+import { defineConfig } from 'eslint/config'
 import * as config from '@lvce-editor/eslint-config'
 
-export default [
+export default defineConfig([
   ...config.default,
   ...config.recommendedActions,
   ...config.recommendedTsconfig,
@@ -43,4 +44,4 @@ export default [
       'sonarjs/prefer-specific-assertions': 'off',
     },
   },
-]
+])
