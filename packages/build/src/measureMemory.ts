1a2091cdc759efcd58b62d4d45bfe0614eac294f
Le Vivilet <levivilet@pm.me>
2026-08-15 08:06:31 +0000

    feature: adjust memory threshold

--- packages/build/src/measureMemory.ts
+++ packages/build/src/measureMemory.ts
@@ -2,7 +2,7 @@ import { measureMemory } from '@lvce-editor/measure-memory'
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-const threshold = 482_000
+const threshold = 484_000
 
 const instantiations = 200_000
 
